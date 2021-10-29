93c8e7f6e26246ad983c52ea30e06863f7ee05eb
antonio <antonio16493@gmail.com>
2021-10-28 19:06:09 -0600

    creacion de model, mapeo, vistas

--- src/main/java/com/umg/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/umg/ecommerce/model/DetalleOrden.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -19,7 +20,7 @@ public class DetalleOrden {
 	private double total;
 	@OneToOne
 	private Orden orden;
-	@OneToOne
+	@ManyToOne
 	private Producto producto;
 	
 	public DetalleOrden() {
